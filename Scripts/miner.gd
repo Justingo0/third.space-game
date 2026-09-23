1e5a2287637601ca55af59641eb975121f3e5804
iamcyrusdaniel <iamcyrusdaniel@gmail.com>
2026-09-23 19:29:12 -0400

    Made it so that miner knows how many ores it's sitting on
    This is useful as we want the miners to generate mined ore based on how many ores it's sitting on!

--- Scripts/miner.gd
+++ Scripts/miner.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+var oreCount = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position = get_global_mouse_position()
+
+func _on_area_entered(area: Area2D) -> void:
+	oreCount += 1
+	print(oreCount)
+
+func _on_area_exited(area: Area2D) -> void:
+	oreCount -= 1
+	print(oreCount)
--- Scripts/test_scene.gd
+++ Scripts/test_scene.gd
@@ -1,4 +1,5 @@
-extends Camera2D
+extends Node2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,6 +7,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-
 func _process(delta: float) -> void:
 	pass
