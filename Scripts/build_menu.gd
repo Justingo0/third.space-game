72ea6743d77e60a011c0c4ad15222849e3dfcd7d
iamcyrusdaniel <iamcyrusdaniel@gmail.com>
2026-09-25 22:27:11 -0400

    Created a UI with 4 different buttons for buildings

---
+++ Scripts/build_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_pressed() -> void:
+	print("button 1 clicked")
+
+func _on_button_2_pressed() -> void:
+	print("button 2 clicked")
+
+func _on_button_3_pressed() -> void:
+	print("button 3 clicked")
+
+func _on_button_4_pressed() -> void:
+	print("button 4 clicked")
