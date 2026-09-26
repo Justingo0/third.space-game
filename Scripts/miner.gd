856e5c0a63b2e3366d03215395a7c55a8a486255
iamcyrusdaniel <iamcyrusdaniel@gmail.com>
2026-09-26 13:27:48 -0400

    Made miners know when to generate ores

--- Scripts/miner.gd
+++ Scripts/miner.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var oreCount = 0
+var mineTime = 0
 var time = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -9,12 +10,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time += delta
+	if oreCount > 0 and time > mineTime:
+		print("mined")
+		time = 0
 
 func _on_area_entered(area: Area2D) -> void:
 	oreCount += 1
-	print(oreCount)
+	mineTime = 4/oreCount
 
 func _on_area_exited(area: Area2D) -> void:
 	oreCount -= 1
-	print(oreCount)
