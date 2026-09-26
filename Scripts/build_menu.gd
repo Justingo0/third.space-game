8c562bd1b0cfa5580d562ab10851a4dd4b45ea99
iamcyrusdaniel <iamcyrusdaniel@gmail.com>
2026-09-26 10:11:14 -0400

    Made the buttons select new buildings

--- Scripts/build_menu.gd
+++ Scripts/build_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
+@onready var camera = $"../../Camera2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,12 +12,17 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	print("button 1 clicked")
+	camera.building_object = load("res://Scenes/miner.tscn")
+	# I want to set the buildcursor building to miner
 
 func _on_button_2_pressed() -> void:
-	print("button 2 clicked")
+	camera.building_object = load("res://Scenes/conveyor.tscn")
+	# I want to set the buildcursor building to conveyor
 
 func _on_button_3_pressed() -> void:
-	print("button 3 clicked")
+	camera.building_object = load("res://Scenes/home.tscn")
+	# I want to set the buildcursor building to core
 
 func _on_button_4_pressed() -> void:
-	print("button 4 clicked")
+	camera.building_object = null
+	# I want to set the buildcursor building to clear
