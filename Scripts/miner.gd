1ce1f888f2cd4215113f3d8e0c2f9d02a243fe90
Justingo0 <justinmalachi1@gmail.com>
2026-09-26 13:32:38 -0400

    Conveyor Item Carrying

--- Scripts/miner.gd
+++ Scripts/miner.gd
@@ -7,8 +7,8 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position = get_global_mouse_position()
+#func _process(delta: float) -> void:
+	#position = get_global_mouse_position()
 
 func _on_area_entered(area: Area2D) -> void:
 	oreCount += 1
